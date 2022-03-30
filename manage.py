d123bbd0d2756083768e3437837fea65a3c080b4
vivek kumar <kumarvivek1752@gmail.com>
2022-03-30 20:45:48 +0530

    created pages and add link

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carezone.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carzone.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
